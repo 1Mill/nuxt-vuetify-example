981815dc0b29b36caad6cc44dc672af6a2a4713b
1Mill <erik.ekberg@outlook.com>
2018-11-04 14:38:44 -0800

    Create TheNav

---
+++ components/TheNav.vue
@@ -0,0 +1,12 @@
+<template>
+	<v-toolbar dark class='[ indigo ]'>
+		<v-toolbar-side-icon/>
+		<v-toolbar-title>Learning Vuetify</v-toolbar-title>
+		<v-spacer/>
+		<v-toolbar-items class="hidden-sm-and-down">
+			<v-btn flat>About</v-btn>
+			<v-btn flat>Contact</v-btn>
+			<v-btn flat>Login</v-btn>
+		</v-toolbar-items>
+	</v-toolbar>
+</template>
