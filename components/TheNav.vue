98637b1c81dfc6b93cebceca362acd0c94cb6dc0
1Mill <erik.ekberg@outlook.com>
2018-11-04 15:52:46 -0800

    Add routes to home and login pages

--- components/TheNav.vue
+++ components/TheNav.vue
@@ -4,9 +4,10 @@
 		<v-toolbar-title>Learning Vuetify</v-toolbar-title>
 		<v-spacer/>
 		<v-toolbar-items>
+			<v-btn flat to='/'>Home</v-btn>
 			<v-btn flat>About</v-btn>
 			<v-btn flat>Contact</v-btn>
-			<v-btn flat>Login</v-btn>
+			<v-btn flat to='/login'>Login</v-btn>
 		</v-toolbar-items>
 	</v-toolbar>
 </template>
