d7e449aa163fd619345eb62e56f46d1b2f45348b
1Mill <erik.ekberg@outlook.com>
2018-11-04 15:50:28 -0800

    Show buttons on smaller screens

--- components/TheNav.vue
+++ components/TheNav.vue
@@ -3,7 +3,7 @@
 		<v-toolbar-side-icon/>
 		<v-toolbar-title>Learning Vuetify</v-toolbar-title>
 		<v-spacer/>
-		<v-toolbar-items class="hidden-sm-and-down">
+		<v-toolbar-items>
 			<v-btn flat>About</v-btn>
 			<v-btn flat>Contact</v-btn>
 			<v-btn flat>Login</v-btn>
