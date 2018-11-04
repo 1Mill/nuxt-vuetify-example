d3eaa634d44fe41327ccdf9833df604d365688c3
1Mill <erik.ekberg@outlook.com>
2018-11-04 14:17:13 -0800

    Add toolbar

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,17 @@
 <template>
 	<div id='app'>
 		<v-app>
+			<v-toolbar dark class='[ indigo ]'>
+				<v-toolbar-side-icon/>
+				<v-toolbar-title>Learning Vuetify</v-toolbar-title>
+				<v-spacer/>
+				<v-toolbar-items class="hidden-sm-and-down">
+					<v-btn flat>About</v-btn>
+					<v-btn flat>Contact</v-btn>
+					<v-btn flat>Login</v-btn>
+				</v-toolbar-items>
+			</v-toolbar>
+
 			<nuxt/>
 		</v-app>
 	</div>
