268d61f056cc57afe3b53b3feabcca839c883c72
1Mill <erik.ekberg@outlook.com>
2018-11-04 14:59:48 -0800

    Add login page / form

---
+++ pages/login.vue
@@ -0,0 +1,20 @@
+<template>
+	<v-container>
+		<v-form v-model="valid">
+			<v-text-field
+			v-model="name"
+			:rules="nameRules"
+			:counter="10"
+			label="Name"
+			required
+			/>
+
+			<v-text-field
+			v-model="email"
+			:rules="emailRules"
+			label="E-mail"
+			required
+			/>
+		</v-form>
+	</v-container>
+</template>
