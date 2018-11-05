2c12ba9b98d6927beca5081d6e284425ec40dd72
1Mill <erik.ekberg@outlook.com>
2018-11-04 22:51:17 -0800

    Add form to login page

--- pages/login.vue
+++ pages/login.vue
@@ -1,20 +1,46 @@
+<script>
+export default {
+	data () {
+		return {
+			valid: false,
+
+			email: null,
+			name: null,
+			password: null
+		}
+	},
+	methods: {
+		submit () {
+			console.log(`${this.name} <${this.email}>, password: ${this.password}`)
+		}
+	}
+}
+</script>
+
 <template>
 	<v-container>
-		<v-form v-model="valid">
+		<v-form @submit.prevent='submit'>
 			<v-text-field
-			v-model="name"
-			:rules="nameRules"
-			:counter="10"
-			label="Name"
+			v-model='name'
+			label='Name'
 			required
 			/>
 
 			<v-text-field
-			v-model="email"
-			:rules="emailRules"
-			label="E-mail"
+			v-model='email'
+			label='E-mail'
 			required
 			/>
+
+			<v-text-field
+			v-model='password'
+			label='Password' type='password'
+			required
+			/>
+
+			<v-btn class='success' type='submit'>
+				Submit
+			</v-btn>
 		</v-form>
 	</v-container>
 </template>
