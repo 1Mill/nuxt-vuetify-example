8be3680dd89e183bfeb3a2ee32dc5be3ab25d6ca
1Mill <erik.ekberg@outlook.com>
2018-11-05 14:38:46 -0800

    Change project name

--- components/TheNav.vue
+++ components/TheNav.vue
@@ -1,7 +1,7 @@
 <template>
 	<v-toolbar dark class='[ indigo ]'>
 		<v-toolbar-side-icon/>
-		<v-toolbar-title>Learning Vuetify</v-toolbar-title>
+		<v-toolbar-title>iTurns Search</v-toolbar-title>
 		<v-spacer/>
 		<v-toolbar-items>
 			<v-btn flat to='/'>Home</v-btn>
