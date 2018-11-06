56008aeb62963577a0736ab021fcdcfb7dbf4de1
1Mill <erik.ekberg@outlook.com>
2018-11-05 22:20:11 -0800

    Add search to index

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,21 @@
+<script>
+export default {
+	data () {
+		return {
+			search: ''
+		}
+	}
+}
+</script>
+
 <template>
-	<v-layout>
-		INDEX
-	</v-layout>
+	<v-container grid-list-md>
+		<v-layout column>
+			<v-flex>
+				<v-text-field label='Search iTunes'
+				v-model='search'
+				/>
+			</v-flex>
+		</v-layout>
+	</v-container>
 </template>
