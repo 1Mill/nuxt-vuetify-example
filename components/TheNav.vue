731727398eb1b53ca7318ea4cf582ed008d27500
1Mill <erik.ekberg@outlook.com>
2018-11-05 22:20:01 -0800

    Update nav

--- components/TheNav.vue
+++ components/TheNav.vue
@@ -1,13 +1,28 @@
+<script>
+export default {
+	data () {
+		return {
+			links: [
+				{name: 'Home', to: '/'},
+				{name: 'Login', to: '/login'},
+			]
+		}
+	}
+}
+</script>
+
 <template>
 	<v-toolbar dark class='[ indigo ]'>
 		<v-toolbar-side-icon/>
 		<v-toolbar-title>iTurns Search</v-toolbar-title>
 		<v-spacer/>
 		<v-toolbar-items>
-			<v-btn flat to='/'>Home</v-btn>
-			<v-btn flat>About</v-btn>
-			<v-btn flat>Contact</v-btn>
-			<v-btn flat to='/login'>Login</v-btn>
+			<v-btn flat
+			v-for='link in links' :key='link.name'
+			:to='link.to'
+			>
+				{{ link.name }}
+			</v-btn>
 		</v-toolbar-items>
 	</v-toolbar>
 </template>
